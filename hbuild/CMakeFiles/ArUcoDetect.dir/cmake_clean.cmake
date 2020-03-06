c711d5332d63fb88db15af4b2be7931bbe744d1e
OllieD3711 <oliver@oliver-Latitude-E6530>
2020-03-06 14:53:46 -0500

    Initial Commit. Checksum compute not verified

---
+++ hbuild/CMakeFiles/ArUcoDetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ArUcoDetect.dir/src/Modules/ArUcoDetect/ArUcoDetect.C.o"
+  "ArUcoDetect.pdb"
+  "ArUcoDetect.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArUcoDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hbuild/CMakeFiles/modinfo_ArUcoDetect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/modinfo_ArUcoDetect"
+  "../src/Modules/ArUcoDetect/modinfo.yaml"
+  "../src/Modules/ArUcoDetect/modinfo.html"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/modinfo_ArUcoDetect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
